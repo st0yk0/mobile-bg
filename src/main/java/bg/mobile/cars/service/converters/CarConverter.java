ebe8fb898176b4e335f466e7723a5b9fb74daf52
st0yk0 <stoyko_mashev@abv.bg>
2020-03-10 22:40:01 +0200

    finishing the converter class

--- src/main/java/bg/mobile/cars/service/converters/CarConverter.java
+++ src/main/java/bg/mobile/cars/service/converters/CarConverter.java
@@ -27,10 +27,40 @@ public class CarConverter {
         model.setEngineType(car.getEngineType());
         model.setGearBox(car.getGearBox());
         model.setCondition(car.getCondition());
-
-
-
+        model.setHorsePower(car.getHorsePower());
+        model.setColor(car.getColor());
+        model.setPrice(car.getPrice());
+        model.setCity(car.getCity());
+        model.setMileage(car.getMileage());
+        model.setUser(car.getUser());
+        model.setExtras(car.getExtras());
 
         return model;
+    }
+
+    public Car convertToEntity(final CarModel model){
+        if(model == null){
+            return null;
+        }
+
+        final Car car = new Car();
+
+
+        car.setId(model.getId());
+        car.setMake(model.getMake());
+        car.setModel(model.getModel());
+        car.setYear(model.getYear());
+        car.setEngineType(model.getEngineType());
+        car.setGearBox(model.getGearBox());
+        car.setCondition(model.getCondition());
+        car.setHorsePower(model.getHorsePower());
+        car.setColor(model.getColor());
+        car.setPrice(model.getPrice());
+        car.setCity(model.getCity());
+        car.setMileage(model.getMileage());
+        car.setUser(model.getUser());
+        car.setExtras(model.getExtras());
+
+        return car;
     }
 }
